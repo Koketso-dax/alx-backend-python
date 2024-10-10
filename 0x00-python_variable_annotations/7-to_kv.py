f4b246c633d87b987b8ec36976442732c1924277
Koketso-dax <lanchaster.k@gmail.com>
2024-10-10 14:49:55 +0200

    Pep8 for 7-kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """ To kv tuple function """
 from typing import List, Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ To kv tuple function """
     return (k, v ** 2)
