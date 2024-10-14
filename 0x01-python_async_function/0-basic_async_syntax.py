bad47689e1d76ad2ce6df9de1d4cc069dc26dc86
Koketso-dax <lanchaster.k@gmail.com>
2024-10-14 21:49:27 +0200

    Remove random seed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,6 @@ async def wait_random(max_delay: int = 10) -> float:
     """ Asynchronous coroutine that waits for a random
     delay between 0 and max_delay seconds. """
 
-    random.seed(888)
     delay = random.uniform(0, float(max_delay))
     await asyncio.sleep(delay)
     return delay
