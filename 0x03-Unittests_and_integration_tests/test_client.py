2fe4f84e89ff593f4b98672745f500a0af6ba7cb
Koketso-dax <lanchaster.k@gmail.com>
2024-11-01 15:57:32 +0200

    [Fix] Use called with once

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,26 +16,27 @@ from unittest.mock import (
 
 class TestGithubOrgClient(unittest.TestCase):
     """Tests for GithubOrgClient"""
+
     @parameterized.expand([
         ("google", {'login': "google"}),
         ("abc", {'login': "abc"})
     ])
-    @patch("client.get_json")
+    @patch("utils.get_json")
     def test_org(self, org: str, resp: Dict,
                  mocked_fxn: MagicMock) -> None:
         """test org method"""
-        mocked_fxn.return_value = MagicMock(return_value=resp)
+
+        mocked_fxn.return_value = resp
         client = GithubOrgClient(org)
-        self.assertEqual(client.org, org)
-        mocked_fxn.assert_called_once_with(
-            f"https://api.github.com/orgs/{org}")
+        client.org()
+        mocked_fxn.called_with_once(client.ORG_URL.format(org=org))
 
     def test_public_repos_url(self) -> None:
         """Tests the _public_repos_url property."""
         with patch("client.GithubOrgClient.org",
                    new_callable=PropertyMock) as mocked_org:
             mocked_org.return_value = {
-                'repos_url': "https://api.github.com/users/google/repos",
+                'repos_url': "https://api.github.com/users/google/repos"
             }
             client = GithubOrgClient("google")
             self.assertEqual(
@@ -43,7 +44,7 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/users/google/repos"
             )
 
-    @patch("client.get_json")
+    @patch("utils.get_json")
     def test_public_repos(self, mocked_fxn: MagicMock) -> None:
         """Tests the public_repos property."""
         mocked_fxn.return_value = MagicMock(return_value=TEST_PAYLOAD)
