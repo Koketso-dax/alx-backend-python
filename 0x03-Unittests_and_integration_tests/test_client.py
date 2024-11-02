79fafe0958c17bf2cdebe4bdfe80799b88b99971
Koketso-dax <lanchaster.k@gmail.com>
2024-11-02 08:38:25 +0200

    [Add] test_has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -49,21 +49,28 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json) -> None:
         """Test GithubOrgClient.public_repos returns the correct value."""
-
         # Arrange
         payloads = [{"name": "google"}, {"name": "Twitter"}]
         mock_get_json.return_value = payloads
         mock_repos_url = "https://api.github.com/orgs/test/repos"
-
         # Act
         with patch('client.GithubOrgClient._public_repos_url',
                    new_callable=PropertyMock) as mock_public:
             mock_public.return_value = mock_repos_url
             test_class = GithubOrgClient('test')
             result = test_class.public_repos()
-
             # Assert
             expected = [x["name"] for x in payloads]
             self.assertEqual(result, expected)
             mock_get_json.assert_called_once_with(mock_repos_url)
             mock_public.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        # Act
+        result = GithubOrgClient.has_license(repo, license_key)
+        # Assert
+        self.assertEqual(result, expected)
